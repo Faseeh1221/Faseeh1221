56940985f92c65d698318845168a2c9026bc16b1
Faseeh1221 <farmanaliqrs@gmail.com>
2024-11-20 21:36:05 +0500

    Add files via upload

---
+++ file.py
@@ -0,0 +1,33 @@
+#arithmetic opreator} +, -, /, *, %, **
+
+a = 10
+b = 2
+
+print(a+b)
+
+#assignment opreator} =, +=, -=, /=, *=, %=, **=
+
+num = 20
+num += 10
+
+print(num)
+
+#realitnal.comparison opreator} ==, !=, <=, >=, <, >
+
+num1 = 50
+num2 = 20
+
+print(num1 == num2)
+
+#logical opreator} not, and, or
+
+a = 20
+b = 10
+
+print(not (a > b))
+
+print((a==b) and (a > b))
+
+print((a < b) or (a > b))
+
+ 
